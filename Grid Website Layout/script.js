77ff43072660bddd9ad3e06340c86668cc6ce656
Vikas Rana <vikas@gmail.com>
2022-07-05 15:30:19 +0530

    Header Complete

--- Grid Website Layout/script.js
+++ Grid Website Layout/script.js
@@ -1,1 +1,8 @@
-script.js
+// || -- HEADER SECTION
+
+$(function () {
+	$(".mob-nav-toggler").click(function(){
+		$(this).toggleClass("nav-open");
+		$(".mob-header-navigation").toggle("slow");
+	});
+});
